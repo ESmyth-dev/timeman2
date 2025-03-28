d12129b7e680dac711a4bff2b88fe2eeca6af14d
bh2013 <bh2013@hw.ac.uk>
2025-03-28 11:43:01 +0000

    Level1 scence

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        openDoor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void openDoor()
+    {
+        Debug.Log("Door Opened");
+
+
+
+    }
+
+
+
+}
+
