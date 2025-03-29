0bdd2d62fcf8bc0dbbde459ba644bd56e9ff185b
bh2013 <bh2013@hw.ac.uk>
2025-03-29 10:47:44 +0000

    ppo

---
+++ Assets/Scripts/LevelEndScript.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CloseDoorScript : MonoBehaviour
+{
+    [SerializeField] private float sceneLoadDelay = 1.0f;
+    [SerializeField] private string nextSceneName = "Level1";
+    private bool levelEnded = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !levelEnded)
+        {
+            Debug.Log("Player has entered the finish room");
+            GameObject[] doors = GameObject.FindGameObjectsWithTag("Finish");
+            StartCoroutine(CloseDoorsAndLoadScene(doors));
+        }
+    }
+
+    private IEnumerator CloseDoorsAndLoadScene(GameObject[] doors)
+    {
+        levelEnded = true;
+
+        // Close all doors
+        foreach (GameObject door in doors)
+        {
+            StartCoroutine(MoveDoor(door.transform, door.transform.position - new Vector3(0, 3, 0), 2.0f));
+        }
+
+        // Wait for doors to finish closing
+        yield return new WaitForSeconds(2.0f);
+
+        // Load scene after delay
+        yield return new WaitForSeconds(sceneLoadDelay);
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+    private IEnumerator MoveDoor(Transform doorTransform, Vector3 targetPosition, float duration)
+    {
+        float time = 0;
+        Vector3 startPosition = doorTransform.position;
+
+        while (time < duration)
+        {
+            doorTransform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        doorTransform.position = targetPosition;
+    }
+}
