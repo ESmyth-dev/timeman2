6be9b3d2f2ffc64edcea5e39fb2d5f1a70ee538b
Ethan Smyth <es2005@hw.ac.uk>
2025-03-31 12:28:06 +0100

    make lasers slow down when in slow mode

--- Assets/LaserMachine/Core/Scripts/LaserMachine.cs
+++ Assets/LaserMachine/Core/Scripts/LaserMachine.cs
@@ -35,15 +35,23 @@ public class LaserMachine : MonoBehaviour {
     LaserProperties m_currentProperties;// = new LaserProperties();
         
     float m_time = 0;
+        float normal_rotation;
+        float slow_rotation;
     bool m_active = true;
     bool m_assignLaserMaterial;
     bool m_assignSparks;
+        PlayerController controller;
   		
 
     void OnEnable()
     {
         m_currentProperties = m_overrideExternalProperties ? m_inspectorProperties : m_data.m_properties;
-        
+            normal_rotation = m_currentProperties.m_rotationSpeed;
+            slow_rotation = normal_rotation * 0.1f;
+            controller = FindAnyObjectByType<PlayerController>();
+
+
+
 
         m_currentProperties.m_initialTimingPhase = Mathf.Clamp01(m_currentProperties.m_initialTimingPhase);
         m_time = m_currentProperties.m_initialTimingPhase * m_currentProperties.m_intervalTime;
@@ -161,10 +169,21 @@ public class LaserMachine : MonoBehaviour {
 
             if ( m_currentProperties.m_rotate )
             {
-                if ( m_currentProperties.m_rotateClockwise )
-                    element.transform.RotateAround(transform.position, transform.up, Time.deltaTime * m_currentProperties.m_rotationSpeed);    //rotate around Global!!
-                else
-                    element.transform.RotateAround(transform.position, transform.up, -Time.deltaTime * m_currentProperties.m_rotationSpeed);
+                if (controller.timeSlowed)
+                    {
+                        if (m_currentProperties.m_rotateClockwise)
+                            element.transform.RotateAround(transform.position, transform.up, Time.deltaTime * slow_rotation);    //rotate around Global!!
+                        else
+                            element.transform.RotateAround(transform.position, transform.up, -Time.deltaTime * slow_rotation);
+                    }
+                    else
+                    {
+                        if (m_currentProperties.m_rotateClockwise)
+                            element.transform.RotateAround(transform.position, transform.up, Time.deltaTime * normal_rotation);
+                        else
+                            element.transform.RotateAround(transform.position, transform.up, -Time.deltaTime * normal_rotation);
+                    }
+
             }
 
 
