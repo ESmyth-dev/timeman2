aa5ad78fc6e82b22e74042213bfd7607192f2656
Ethan Smyth <es2005@hw.ac.uk>
2025-03-31 18:47:04 +0100

    add images to level up screen

--- Assets/Scripts/SkillCanvasPopulator.cs
+++ Assets/Scripts/SkillCanvasPopulator.cs
@@ -15,6 +15,7 @@ public class SkillCanvasPopulator : MonoBehaviour
     public Skill skill2;
     public Skill skill3;
 
+    private Color iconColor;
     private Transform Button1;
     private Transform Button2;
     private Transform Button3;
@@ -26,6 +27,11 @@ public class SkillCanvasPopulator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {  
+        if (UnityEngine.ColorUtility.TryParseHtmlString("#00FFEF", out Color color))
+            {
+                iconColor = color;
+            }
+
         description = GameObject.Find("Description").transform;
         ender = FindAnyObjectByType<CloseDoorScript>();
         
@@ -42,6 +48,8 @@ public class SkillCanvasPopulator : MonoBehaviour
         Button1 = GameObject.Find("ButtonShell").transform;
         Button1.GetComponentInChildren<TMP_Text>().text = skill1.skillName;
         Button1.GetComponent<SkillButton>().skillDescription = skill1.skillDescription;
+        Button1.GetComponentInChildren<Image>().sprite = skill1.skillTexture;
+        Button1.GetComponentInChildren<Image>().color = iconColor;
         Button1.GetComponentInChildren<Button>().onClick.AddListener(() =>
         {
             skill1.isUnlocked = true;
@@ -55,6 +63,8 @@ public class SkillCanvasPopulator : MonoBehaviour
         Button2 = GameObject.Find("ButtonShell (1)").transform;
         Button2.GetComponentInChildren<TMP_Text>().text = skill2.skillName;
         Button2.GetComponent<SkillButton>().skillDescription = skill2.skillDescription;
+        Button2.GetComponentInChildren<Image>().sprite = skill2.skillTexture;
+        Button2.GetComponentInChildren<Image>().color = iconColor;
         Button2.GetComponentInChildren<Button>().onClick.AddListener(() =>
         {
             skill2.isUnlocked = true;
@@ -67,6 +77,8 @@ public class SkillCanvasPopulator : MonoBehaviour
         Button3 = GameObject.Find("ButtonShell (2)").transform;
         Button3.GetComponentInChildren<TMP_Text>().text = skill3.skillName;
         Button3.GetComponent<SkillButton>().skillDescription = skill3.skillDescription;
+        Button3.GetComponentInChildren<Image>().sprite = skill3.skillTexture;
+        Button3.GetComponentInChildren<Image>().color = iconColor;
         Button3.GetComponentInChildren<Button>().onClick.AddListener(() =>
         {
             skill3.isUnlocked = true;
