2576ecab8752531376c31fd8cc1ec9e5db41f00a
bh2013 <bh2013@hw.ac.uk>
2025-03-31 19:41:05 +0100

    push

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -159,9 +159,13 @@ public class PlayerController : MonoBehaviour
                 Hit();
             }
         }
-   
-
-
+    
+    private void OnParticleCollision(GameObject particleSystemGO)
+    {
+        // Check if the colliding particle's GameObject has the "Laser" tag
+        Hit();
+    
+    }
     // Update is called once per frame
     void Update()
     {
