99f129d992f51d7c6d3a33ff77b9533c5b2f95dc
Ethan Smyth <es2005@hw.ac.uk>
2025-03-31 17:58:23 +0100

    make the crosshair blue

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -453,7 +453,7 @@ public class PlayerController : MonoBehaviour
 
         if (blinkReady && blinkVector != Vector3.zero)
         {
-            if (Physics.Raycast(transform.position + transform.up * 0.5f, blinkVector, out RaycastHit hit, blinkDistance, LayerMask.GetMask(new string[] {"LevelLineOfSight", "Level"})))
+            if (Physics.Raycast(transform.position + transform.up * 0.7f, blinkVector, out RaycastHit hit, blinkDistance, LayerMask.GetMask(new string[] {"LevelLineOfSight", "Level"})))
             {
                 Debug.Log("Obstacle detected! shorter teleport");
                 transform.position += blinkVector * (hit.distance - 1.5f);
