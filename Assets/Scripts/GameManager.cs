bee1d59392f2c965a5db393f130a7ac52fd7945f
bh2013 <bh2013@hw.ac.uk>
2025-04-01 09:53:55 +0100

    ps

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -32,7 +32,7 @@ public class GameManager : MonoBehaviour
 
     public bool deathBubble = false;
 
-    public int NUM_ROOMS_FOR_WIN = 3;
+    public int NUM_ROOMS_FOR_WIN = 5;
 
     public List<string> levels;
 
